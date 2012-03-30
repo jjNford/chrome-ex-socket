07a822985f49b6c83020d916b2f0dda2ec941b32
JJ Ford <jj.n.ford@gmail.com>
2012-03-29 23:30:28 -0500

    Include example of chrome-extension-socket.

---
+++ example/js/script.js
@@ -0,0 +1,22 @@
+// Show loading background.
+function showLoading() {
+	$('.count').addClass('loading');
+}
+
+// Hide loading background.
+function hideLoading() {
+	$('.count').removeClass('loading');
+}
+
+// Update the task counter.
+function updateTaskCount(count) {
+	count = count == 0 ? "No Tasks Running" : count;
+	$('.count').text(count);
+}
+
+// When document is ready create click bindings.
+$(document).ready( function() {
+	$('.add_task').on('click', function() {
+		Socket.postTask({type: "task"});
+	});
+});
